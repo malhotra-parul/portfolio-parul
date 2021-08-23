13878539381a3a49a6cf87a207dcf409bb2aac20
malhotra_parul <parulm@geekyants.com>
2021-08-23 12:43:26 +0530

    added 2021 Projects

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -21,33 +21,45 @@ export const Skills = () => {
               I enjoy keeping this personal timeline up to date. It highlights some of the important moments from my
               life each year.
             </p>
+            <h5>2021</h5>
+            <p>1. Started working with a UK based travel company <a href="https://tripabrood.com/">TripAbrood</a> as a Backend Developer.
+            </p>
+            <p>
+              2. Worked with an Indian startup <a href="https://khatabook.com/">Khatabook</a> as a Backend NodeJs developer.
+            </p>
+            <p>
+              2.
+            </p>
             <h5>2020</h5>
             <p>
-              1. Built a website to analyze the <a href="https://github.com/malhotra-parul/schools-connectivity-data-viz-react">connectivity of schools across Bangalore</a>, on the basis of parameters like connectivity with bus routes using openCity dataset. Visualization done using React. Backend built using MongoDb.
+              1. Joined <a href="https://geekyants.com/">GeekyAnts</a> as a Full Stack Developer.
             </p>
             <p>
-              2. Built a <a href="https://dashboard-in-react.vercel.app/">dashboard</a> in React.
+              2. Built a website to analyze the <a href="https://github.com/malhotra-parul/schools-connectivity-data-viz-react">connectivity of schools across Bangalore</a>, on the basis of parameters like connectivity with bus routes using openCity dataset. Visualization done using React. Backend built using MongoDb.
             </p>
             <p>
-              3. 🐦 Built a{' '}
+              3. Built a <a href="https://dashboard-in-react.vercel.app/">dashboard</a> in React.
+            </p>
+            <p>
+              4. 🐦 Built a{' '}
               <a href="https://documenter.getpostman.com/view/7194332/T17DiA1C?version=latest">twitter clone</a> backend
               using express and firebase functions.
             </p>
             <p>
-              4. 💬 Built a <a href="https://devslack-be53b.web.app/">slack clone in React.</a>
+              5. 💬 Built a <a href="https://devslack-be53b.web.app/">slack clone in React.</a>
             </p>
             <p>
-              5. An article I wrote for <a href="https://medium.com/p/99432310d476">Better Programming</a> received 21k
+              6. An article I wrote for <a href="https://medium.com/p/99432310d476">Better Programming</a> received 21k
               views.
             </p>
             <p>
-              6. 💻 Built a <a href="https://codeplayground.netlify.app/">coding playground in React</a> for a kids
+              7. 💻 Built a <a href="https://codeplayground.netlify.app/">coding playground in React</a> for a kids
               bootcamp.
             </p>
             <h5>2019</h5>
-            <p>7. Joined an online bootcamp to pursue my interest in development.</p>
+            <p>1. Joined an online bootcamp to pursue my interest in development.</p>
             <p>
-              8. Joined <em>Milkbasket.com</em> as an SDET.
+              2. Joined <em>Milkbasket.com</em> as an SDET.
             </p>
           </div>
           <Button as={AnchorLink} href="#contact">
